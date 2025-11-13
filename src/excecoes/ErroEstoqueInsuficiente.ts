5ec6ee6d2ac8de8321706f09a95debaa4811c3fd
Claudiomir Neves <73174868+diomids2@users.noreply.github.com>
2025-11-13 18:22:09 -0300

    Delete src/excecoes directory

--- src/excecoes/ErroEstoqueInsuficiente.ts
+++
@@ -1,6 +0,0 @@
-export class ErroEstoqueInsuficiente extends Error {
-  constructor(produto: string, solicitado: number, disponivel: number) {
-    super(`Estoque insuficiente para '${produto}'. Solicitado: ${solicitado}, Disponível: ${disponivel}`);
-    this.name = "ErroEstoqueInsuficiente";
-  }
-}
--- src/excecoes/ErroProdutoNaoEncontrado.ts
+++
@@ -1,6 +0,0 @@
-export class ErroProdutoNaoEncontrado extends Error {
-  constructor(id: number) {
-    super(`Produto com id ${id} nao encontrado.`);
-    this.name = "ErroProdutoNaoEncontrado";
-  }
-}
