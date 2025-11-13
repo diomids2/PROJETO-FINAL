42c423eb73432e30c0ffde6e51169516ed3f0145
Claudiomir Neves <73174868+diomids2@users.noreply.github.com>
2025-11-13 17:29:52 -0300

    Delete src directory

--- src/pagamento/PagamentoPix.ts
+++
@@ -1,8 +0,0 @@
-import { EstrategiaPagamento } from "./EstrategiaPagamento";
-export class PagamentoPix implements EstrategiaPagamento {
-  constructor(private readonly chave: string) {}
-  pagar(valor: number): boolean {
-    console.log(`PIX (${this.chave}) - R$ ${valor.toFixed(2)}: aprovado`);
-    return true;
-  }
-}
